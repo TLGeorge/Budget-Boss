d043c86a92ee4dbb777da14ac31d94c9c78672be
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-07-06 16:59:09 -0400

    edit db.js

--- public/db.js
+++ public/db.js
@@ -3,13 +3,15 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-
+    // create object store called "pending" and set autoIncrement to true
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
     db = event.target.result;
+
+    // check if app is online before reading from db
     if (navigator.onLine) {
         checkDatabase();
     }
@@ -20,15 +22,22 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
-
+    // create a transaction on the pending db with readwrite access
     const transaction = db.transaction(["pending"], "readwrite");
+
+    // access your pending object store
     const store = transaction.objectStore("pending");
+
+    // add record to your store with add method.
     store.add(record);
 }
 
 function checkDatabase() {
+    // open a transaction on your pending db
     const transaction = db.transaction(["pending"], "readwrite");
+    // access your pending object store
     const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
     const getAll = store.getAll();
 
     getAll.onsuccess = function () {
@@ -43,10 +52,13 @@ function checkDatabase() {
             })
                 .then(response => response.json())
                 .then(() => {
+                    // if successful, open a transaction on your pending db
                     const transaction = db.transaction(["pending"], "readwrite");
 
+                    // access your pending object store
                     const store = transaction.objectStore("pending");
 
+                    // clear all items in your store
                     store.clear();
                 });
         }
